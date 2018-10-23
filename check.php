581d1eb7e0d737b093512964d8a976c4df5073fd
听风 <38204020+lychs1998@users.noreply.github.com>
2018-10-23 17:56:51 +0800

    2018.10.23更新

--- check.php
+++ check.php
@@ -1,12 +1,25 @@
 <?php
 $token=$_POST['token'];
 $key=$_POST['key'];
-if(file_get_contents("$token.txt")==$key){
-$arr = ['code' => 1, 'msg' => '登录成功'];
-unlink("./$token.txt");
+$u=$_POST['url'];
+$url="404";
+if(file_exists("$token.check")){
+   if(file_get_contents("$token.check")==$key){
+      $json = file_get_contents('url.json');
+      $urllist=json_decode($json,true);
+      foreach($urllist as $ul){
+         if($ul['key']==$u){
+	      $url=$ul['url'];
+      }
+}
+$arr = ['code' => 1, 'msg' => '登录成功','url'=>$url];
+unlink("./$token.check");
 }
 else{
    $arr = ['code' => 500, 'msg' => 'ERROR'];
+}
+}else{
+   $arr = ['code' => 0, 'msg' => 'ERROR'];
 }
 echo json_encode($arr);
 exit();
